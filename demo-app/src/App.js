0baa6cff4697778cfb3cfee8a3624c75d2d3f129
saqlain shah <cyedcaqlain1@gmail.com>
2023-06-12 20:46:06 +0500

    React App Basic Structure

---
+++ demo-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Counter from "./components/counter";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Counter />
+    </React.Fragment>
+  );
+}
+
+export default App;
