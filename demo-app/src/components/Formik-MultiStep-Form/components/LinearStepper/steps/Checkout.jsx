46a2a56ab87a7ef596ccb1fba680a3a6f8e14280
Jaffer <mjaffer720@gmail.com>
2023-08-23 20:26:15 +0500

    refactoring

--- demo-app/src/components/Formik-MultiStep-Form/components/LinearStepper/steps/Checkout.jsx
+++ demo-app/src/components/Formik-MultiStep-Form/components/LinearStepper/steps/Checkout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { styled } from "@material-ui/core/styles";
+import { styled } from "@mui/styles";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell, { tableCellClasses } from "@mui/material/TableCell";
